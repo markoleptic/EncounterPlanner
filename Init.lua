c65aa06ded06bb1180e6587c77b438513e11d366
markoleptic <markalexcham@gmail.com>
2024-10-19 23:16:03 -0600

    Add font

--- Init.lua
+++ Init.lua
@@ -6,16 +6,20 @@ local Private = select(2, ...)
 
 do
 	Private.AddOn = LibStub("AceAddon-3.0"):NewAddon(AddOnName, "AceConsole-3.0", "AceEvent-3.0")
-	Private.AddOn.OptionsModule = Private.AddOn:NewModule("Options", "AceConsole-3.0")
+	Private.AddOn.OptionsModule = Private.AddOn:NewModule("Options", "AceConsole-3.0") --[[@as OptionsModule]]
 	Private.Libs = {}
 	Private.Libs.ACD = LibStub("AceConfigDialog-3.0")
 	Private.Libs.AC = LibStub("AceConfig-3.0");
 	Private.Libs.ACR = LibStub("AceConfigRegistry-3.0")
 	Private.Libs.ADBO = LibStub("AceDBOptions-3.0")
 	Private.Libs.LSM = LibStub("LibSharedMedia-3.0")
+	Private.Libs.LSM:Register("font", "PT Sans Narrow",
+		"Interface\\Addons\\EncounterPlanner\\Media\\Fonts\\PTSansNarrow-Bold.ttf",
+		bit.bor(Private.Libs.LSM.LOCALE_BIT_western, Private.Libs.LSM.LOCALE_BIT_ruRU))
 	Private.Libs.AGUI = LibStub("AceGUI-3.0")
+	Private.combatLogEventBasedTimers = {} --[[@as CombatLogEventBasedTimers]]
+	Private.absoluteTimeBasedTimers = {} --[[@as table<integer, table<number, table<integer, AbsoluteTimeBasedTimer>>>]]
 	Private.phaseBasedTimers = {}
-	Private.absoluteTimeBasedTimers = {}
 	Private.customTimers = {}
 	Private.lastEncounterId = nil
 	Private.selectedBoss = nil
