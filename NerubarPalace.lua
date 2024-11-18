34401c2f271497f99d4dce58eb7dc61bea16dd8e
markoleptic <markalexcham@gmail.com>
2024-11-17 18:13:01 -0700

    Implement The Silken Court

--- NerubarPalace.lua
+++ NerubarPalace.lua
@@ -159,6 +159,9 @@ local bosses = {
 			}),
 		},
 	}),
+	-- ["The Bloodbound Horror"] = Private.classes.Boss:New({}),
+	-- ["Sikran, Captain of the Sureki"] = Private.classes.Boss:New({}),
+	-- ["Rasha'nan"] = Private.classes.Boss:New({}),
 	["Broodtwister Ovi'nax"] = Private.classes.Boss:New({
 		abilities = {
 			[441362] = Private.classes.BossAbility:New({ -- Volatile Concoction
@@ -325,6 +328,276 @@ local bosses = {
 			}),
 		},
 	}),
+	["The Silken Court"] = Private.classes.Boss:New({
+		abilities = {
+			[440504] = Private.classes.BossAbility:New({ -- Impaling Eruption
+				phases = {
+					[1] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 8.0, 20.0, 34.0, 20.0 },
+						repeatInterval = nil,
+					}),
+					[3] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 11.0, 30.0, 30.0, 30.0 },
+						repeatInterval = nil,
+					}),
+				},
+				castTime = 4.5,
+				duration = 0.0,
+			}),
+			[438218] = Private.classes.BossAbility:New({ -- Piercing Strike
+				phases = {
+					[1] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 13.0, 20.0, 27.0, 20.0 },
+						repeatInterval = nil,
+					}),
+					[3] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 16.0, 20.0, 25.0, 15.0, 20.0, 25.0 },
+						repeatInterval = nil,
+					}),
+					[5] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 20.0, 17.0, 32.0, 20.0, 21.0, 20.0, 36.0 },
+						repeatInterval = nil,
+					}),
+				},
+				castTime = 1.5,
+				duration = 0.0,
+			}),
+			[438801] = Private.classes.BossAbility:New({ -- Call of the Swarm
+				phases = {
+					[1] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 25.0, 53.0 },
+						repeatInterval = nil,
+					}),
+					[3] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 31.0, 61.0 },
+						repeatInterval = nil,
+					}),
+				},
+				castTime = 3,
+				duration = 0.0,
+			}),
+			[441791] = Private.classes.BossAbility:New({ -- Burrowed Eruption
+				phases = {
+					[1] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 35.0, 60.0 },
+						repeatInterval = nil,
+					}),
+					[5] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 43.0, 98.0 },
+						repeatInterval = nil,
+					}),
+				},
+				castTime = 1.8,
+				duration = 0.0,
+			}),
+			[440246] = Private.classes.BossAbility:New({ -- Reckless Charge
+				phases = {
+					[1] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 38.5, 60.0 },
+						repeatInterval = nil,
+					}),
+					[5] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 46.1, 98.0 },
+						repeatInterval = nil,
+					}),
+				},
+				castTime = 1.8,
+				duration = 0.0,
+			}),
+			[450045] = Private.classes.BossAbility:New({ -- Skittering Leap
+				phases = {
+					[1] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 42.1, 60.0 },
+						repeatInterval = nil,
+					}),
+				},
+				castTime = 1.0,
+				duration = 0.0,
+			}),
+			[439838] = Private.classes.BossAbility:New({ -- Web Bomb
+				phases = {
+					[1] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 16.7, 70.2 },
+						repeatInterval = nil,
+					}),
+				},
+				castTime = 1.5,
+				duration = 0.0,
+			}),
+			[438656] = Private.classes.BossAbility:New({ -- Venomous Rain
+				phases = {
+					[1] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 19.8, 33.3, 26.8 },
+						repeatInterval = nil,
+					}),
+				},
+				castTime = 1.5,
+				duration = 10.0,
+			}),
+			[438677] = Private.classes.BossAbility:New({ -- Stinging Swarm
+				phases = {
+					[3] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 25.0, 58.0 },
+						repeatInterval = nil,
+					}),
+					[5] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 81.1, 57.0 },
+						repeatInterval = nil,
+					}),
+				},
+				castTime = 2.0,
+				duration = 0.0,
+			}),
+			[441782] = Private.classes.BossAbility:New({ -- Strands of Reality
+				phases = {
+					[3] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 32.0, 36.0, 24.0 },
+						repeatInterval = nil,
+					}),
+					[5] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 22.2, 33.7, 24.9, 43.0, 33.8, 24.9, 43.0 },
+						repeatInterval = nil,
+					}),
+				},
+				castTime = 4.5,
+				duration = 0.0,
+			}),
+			[450483] = Private.classes.BossAbility:New({ -- Void Step
+				phases = {
+					[3] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 38.7, 34.2, 23.6, 29.2 },
+						repeatInterval = nil,
+					}),
+					[5] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 49.3, 40.5, 29.1, 30.1, 2.6, 38.4, 29.2, 30.1, 2.7 },
+						repeatInterval = nil,
+					}),
+				},
+				castTime = 1.0,
+				duration = 0.0,
+			}),
+			[441626] = Private.classes.BossAbility:New({ -- Web Vortex
+				phases = {
+					[3] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 20.2, 55.8 },
+						repeatInterval = nil,
+					}),
+					[5] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 33.4, 33.7, 63.8, 33.7, 64.0 },
+						repeatInterval = nil,
+					}),
+				},
+				castTime = 2.0,
+				duration = 16.0,
+			}),
+			[450129] = Private.classes.BossAbility:New({ -- Entropic Desolation
+				phases = {
+					[3] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 25.4, 55.8 },
+						repeatInterval = nil,
+					}),
+					[5] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 38.7, 33.7, 63.8, 33.7, 64.0 },
+						repeatInterval = nil,
+					}),
+				},
+				castTime = 4.5,
+				duration = 8.0,
+			}),
+			[438355] = Private.classes.BossAbility:New({ -- Cataclysmic Entropy
+				phases = {
+					[3] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 41.7, 57.9 },
+						repeatInterval = nil,
+					}),
+					[5] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 92.7, 61.8 },
+						repeatInterval = nil,
+					}),
+				},
+				castTime = 10.0,
+				duration = 0.0,
+			}),
+			[463459] = Private.classes.BossAbility:New({ -- Apex of Entropy
+				phases = {
+					[5] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 170.0 },
+						repeatInterval = nil,
+					}),
+				},
+				castTime = 3.0,
+				duration = 8.0,
+			}),
+			[443598] = Private.classes.BossAbility:New({ -- Uncontrollable Rage
+				phases = {
+					[5] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 170.0 },
+						repeatInterval = nil,
+					}),
+				},
+				castTime = 3.0,
+				duration = 8.0,
+			}),
+			[450980] = Private.classes.BossAbility:New({ -- Shatter Existence
+				phases = {
+					[2] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 0.0 },
+						repeatInterval = nil,
+					}),
+				},
+				castTime = 45.0,
+				duration = 0.0,
+			}),
+			[451277] = Private.classes.BossAbility:New({ -- Spike Storm
+				phases = {
+					[4] = Private.classes.BossAbilityPhase:New({
+						castTimes = { 0.0 },
+						repeatInterval = nil,
+					}),
+				},
+				castTime = 45.0,
+				duration = 0.0,
+			}),
+		},
+		phases = {
+			[1] = Private.classes.BossPhase:New({ -- Stage 1: Clash of Rivals
+				duration = 127,
+				defaultDuration = 127,
+				count = 1,
+				defaultCount = 1,
+				repeatAfter = nil,
+			}),
+			[2] = Private.classes.BossPhase:New({ -- Intermission: Void Ascension
+				duration = 45,
+				defaultDuration = 45,
+				count = 1,
+				defaultCount = 1,
+				repeatAfter = nil,
+			}),
+			[3] = Private.classes.BossPhase:New({ -- Stage 2: Grasp of the Void
+				duration = 131,
+				defaultDuration = 131,
+				count = 1,
+				defaultCount = 1,
+				repeatAfter = nil,
+			}),
+			[4] = Private.classes.BossPhase:New({ -- Intermission: Raging Fury
+				duration = 45,
+				defaultDuration = 45,
+				count = 1,
+				defaultCount = 1,
+				repeatAfter = nil,
+			}),
+			[5] = Private.classes.BossPhase:New({ -- Stage 3: Unleashed Rage
+				duration = 252,
+				defaultDuration = 252,
+				count = 1,
+				defaultCount = 1,
+				repeatAfter = nil,
+			}),
+		},
+	}),
+	-- ["Queen Ansurek"] = Private.classes.Boss:New({}),
 }
 
 -- Generate a list of abilities for each boss sorted by their first cast time
