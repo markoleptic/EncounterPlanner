b1469e070ce28038a193ee72ad73bd56366750d2
markoleptic <markalexcham@gmail.com>
2024-12-07 17:47:20 -0700

    Allow spacer to be used as a line

--- Widgets/EPSpacer.lua
+++ Widgets/EPSpacer.lua
@@ -8,18 +8,21 @@ local CreateFrame = CreateFrame
 local defaultHeight = 4
 
 ---@class EPSpacer : AceGUIWidget
----@field frame table|Frame
+---@field frame table|Frame|BackdropTemplate
 ---@field type string
 ---@field fillSpace boolean
 
 ---@param self EPSpacer
 local function OnAcquire(self)
+	self.frame:Show()
 	self:SetHeight(defaultHeight)
 	self.fillSpace = false
 end
 
 ---@param self EPSpacer
-local function OnRelease(self) end
+local function OnRelease(self)
+	self.frame:ClearBackdrop()
+end
 
 local function SetFillSpace(self, fill)
 	self.fillSpace = fill
@@ -27,7 +30,7 @@ end
 
 local function Constructor()
 	local count = AceGUI:GetNextWidgetNum(Type)
-	local frame = CreateFrame("Frame", Type .. count, UIParent)
+	local frame = CreateFrame("Frame", Type .. count, UIParent, "BackdropTemplate")
 	frame:SetWidth(1)
 	frame:Hide()
 
