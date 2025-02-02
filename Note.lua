ed85c64903e94850d64d24b8624c42f09ad54e20
markoleptic <markalexcham@gmail.com>
2025-02-01 18:18:50 -0700

    Use message log to issue import warnings and errors
    - Plans are now required to use all combat log event spell IDs from the same boss. The most common will be used to determine which boss.
    - Remove requirement of having no dashes in assignment lines.
    - Adjusted assignment line parsing to extract target names for each assignee of a comma separated list.

--- Note.lua
+++ Note.lua
@@ -3,6 +3,7 @@ local AddOnName, Namespace = ...
 ---@class Private
 local Private = Namespace
 local AddOn = Private.addOn
+local L = Private.L
 ---@class Assignment
 local Assignment = Private.classes.Assignment
 ---@class CombatLogEventAssignment
@@ -55,14 +56,14 @@ local colorStartRegex = "|?|c........"
 local nameRegex = "^(%S+)"
 local nonSymbolRegex = "[^ \n,%(%)%[%]_%$#@!&]+"
 local phaseNumberRegex = "^p(g?):?(.-)$"
-local postDashRegex = "([^ \n-][^\n-]-)  +"
+local postDashRegex = "([^ \n][^\n]-)  +"
 -- postDashRegex = "([%w:,%s|%- ]+[ ]+%b{}[ ]-)"
 local postOptionsPreDashNoSpellRegex = "}(.-) %-"
 local postOptionsPreDashRegex = "}{spell:(%d+)}?(.-) %-"
 local removeFirstDashRegex = "%-+%s-([^\n]+)"
 local spellIconRegex = "{spell:(%d+):?%d*}"
 local stringWithoutSpellRegex = "(.*){spell:(%d+):?%d*}"
-local targetNameRegex = "@(%S+)"
+local targetNameRegex = "(@%S+)"
 local textRegex = "{[Tt][Ee][Xx][Tt]}(.-){/[Tt][Ee][Xx][Tt]}"
 local timeOptionsSplitRegex = "{time:(%d+)[:%.]?(%d*),?([^{}]*)}"
 local combatLogEventFromAbbreviation = {
@@ -72,17 +73,30 @@ local combatLogEventFromAbbreviation = {
 	["SAR"] = "SPELL_AURA_REMOVED",
 }
 
+---@alias OptionFailureReason
+---|1 Invalid assignment type
+---|2 Invalid combat log event type
+---|3 Invalid combat log event spell ID
+---|4 Invalid combat log event spell count
+---|5 No spell count
+---|6 Invalid assignee name or role
+---|7 Invalid boss
+
+---@class FailureTableEntry
+---@field reason OptionFailureReason
+---@field string string
+---@field replacedSpellCount? integer
+
 -- Parses a line of text in the note and creates assignment(s).
 ---@param line string
+---@param failed table<integer, FailureTableEntry>
 ---@return table<integer, Assignment>
-local function CreateAssignmentsFromLine(line)
+---@return integer
+local function CreateAssignmentsFromLine(line, failed)
 	local assignments = {}
+	local failedCount = 0
 	local removedFirstDash = line:match(removeFirstDashRegex)
-	for str in (removedFirstDash):gmatch(postDashRegex) do
-		local targetName = str:match(targetNameRegex)
-		if targetName then
-			targetName = targetName:gsub(colorStartRegex, ""):gsub(colorEndRegex, "")
-		end
+	for str in (removedFirstDash .. "  "):gmatch(postDashRegex) do
 		local spellInfo =
 			{ name = "", iconID = 0, originalIconID = 0, castTime = 0, minRange = 0, maxRange = 0, spellID = 0 }
 		local strWithoutSpell = str:gsub(stringWithoutSpellRegex, function(rest, id)
@@ -95,29 +109,103 @@ local function CreateAssignmentsFromLine(line)
 			text = text:gsub("^%s*", ""):gsub("$^%s*", "") -- remove beginning/trailing whitespace
 			strWithoutSpell = strWithoutSpell:gsub(textRegex, "")
 		end
-		local nameOrGroup = strWithoutSpell:match(assigneeGroupRegex) or strWithoutSpell:match(nameRegex)
-		if nameOrGroup then
-			for _, entry in pairs(splitTable(",", nameOrGroup)) do
-				entry = entry:gsub(colorStartRegex, ""):gsub(colorEndRegex, "")
-				local assigneeNameOrRole = IsValidAssigneeNameOrRole(entry)
-				if assigneeNameOrRole then
-					local assignment = Assignment:New({
-						assigneeNameOrRole = assigneeNameOrRole,
-						text = text,
-						spellInfo = spellInfo,
-						targetName = targetName,
-					})
-					if assignment.spellInfo.spellID == kInvalidAssignmentSpellID then
-						if assignment.text:len() > 0 then
-							assignment.spellInfo.spellID = kTextAssignmentSpellID
-						end
-					end
-					tinsert(assignments, assignment)
+		for _, entry in pairs(splitTable(",", strWithoutSpell:gsub("%s", ""))) do
+			entry = entry:gsub(colorStartRegex, ""):gsub(colorEndRegex, "") -- Remove colors
+			local targetName = nil
+			entry = entry:gsub(targetNameRegex, function(target)
+				if target then
+					targetName = target:gsub("@", "") -- Extract target name
 				end
+				return "" -- Remove match
+			end)
+			local assigneeNameOrRole = IsValidAssigneeNameOrRole(entry)
+			if assigneeNameOrRole then
+				local assignment = Assignment:New({
+					assigneeNameOrRole = assigneeNameOrRole,
+					text = text,
+					spellInfo = spellInfo,
+					targetName = targetName,
+				})
+				if assignment.spellInfo.spellID == kInvalidAssignmentSpellID then
+					if assignment.text:len() > 0 then
+						assignment.spellInfo.spellID = kTextAssignmentSpellID
+					end
+				end
+				tinsert(assignments, assignment)
+			else
+				tinsert(failed, { reason = 6, string = entry })
+				failedCount = failedCount + 1
 			end
 		end
+
+		-- elseif strWithoutSpell:gsub("%s", ""):len() > 0 then
+		-- 	tinsert(failed, { reason = 6, string = strWithoutSpell })
+		-- 	failedCount = failedCount + 1
+		-- end
 	end
-	return assignments
+	return assignments, failedCount
+end
+
+---@param option string
+---@param time number
+---@param assignments table<integer, Assignment>
+---@param derivedAssignments table<integer, Assignment>
+---@param replaced table<integer, FailureTableEntry>
+---@param encounterIDs table<integer, {assignmentIDs: table<integer, integer>, string: string}> Boss encounter spell IDs
+---@return boolean -- True if combat log event assignments were added
+---@return boolean -- True if first first return value is true and replaced invalid spell count
+---@return boolean -- True if invalid combat log event type or combat log event spell ID
+local function ProcessCombatEventLogEventOption(option, time, assignments, derivedAssignments, replaced, encounterIDs)
+	local combatLogEventAbbreviation, spellIDStr, spellCountStr, rest = split(":", option, 4)
+	if combatLogEventFromAbbreviation[combatLogEventAbbreviation] then
+		local spellID = tonumber(spellIDStr)
+		local spellCount = tonumber(spellCountStr)
+		if spellID then
+			local bossDungeonEncounterID = GetBossDungeonEncounterIDFromSpellID(spellID)
+			if bossDungeonEncounterID then
+				encounterIDs[bossDungeonEncounterID] = encounterIDs[bossDungeonEncounterID]
+					or { assignmentIDs = {}, string = option }
+				local replacedInvalidSpellCount = false
+				if spellCount then
+					if not IsValidSpellCount(bossDungeonEncounterID, spellID, spellCount) then
+						spellCount = GetMaxSpellCount(bossDungeonEncounterID, spellID)
+						tinsert(replaced, {
+							reason = 4,
+							string = option,
+							replacedSpellCount = spellCount,
+						})
+						replacedInvalidSpellCount = true
+					end
+				else
+					tinsert(replaced, { reason = 5, string = option })
+					replacedInvalidSpellCount = true
+					spellCount = 1
+				end
+				if spellCount then
+					for _, assignment in pairs(assignments) do
+						local combatLogEventAssignment = CombatLogEventAssignment:New(assignment)
+						combatLogEventAssignment.combatLogEventType = combatLogEventAbbreviation
+						combatLogEventAssignment.time = time
+						combatLogEventAssignment.spellCount = spellCount
+						combatLogEventAssignment.combatLogEventSpellID = spellID
+						tinsert(derivedAssignments, combatLogEventAssignment)
+						tinsert(encounterIDs[bossDungeonEncounterID].assignmentIDs, combatLogEventAssignment.uniqueID)
+					end
+					return true, replacedInvalidSpellCount, false
+				end
+			else
+				tinsert(replaced, { reason = 3, string = option })
+				return false, false, true
+			end
+		else
+			tinsert(replaced, { reason = 3, string = option })
+			return false, false, true
+		end
+	elseif combatLogEventAbbreviation and combatLogEventAbbreviation:gsub("%s", ""):len() > 0 then
+		tinsert(replaced, { reason = 2, string = option })
+		return false, false, true
+	end
+	return true, false, false
 end
 
 -- Adds an assignment using a more derived type by parsing the options (comma-separated list after time).
@@ -125,62 +213,51 @@ end
 ---@param derivedAssignments table<integer, Assignment>
 ---@param time number
 ---@param options string
-local function ProcessOptions(assignments, derivedAssignments, time, options)
+---@param replaced table<integer, FailureTableEntry>
+---@param encounterIDs table<integer, {assignmentIDs: table<integer, integer>, string: string}>
+---@return integer -- defaultedToTimedAssignmentCount
+---@return integer -- defaultedToNearestSpellCountCount
+local function ProcessOptions(assignments, derivedAssignments, time, options, replaced, encounterIDs)
 	local regularTimer = true
-	local option = nil
-	while options do
-		option, options = split(",", options, 2)
-		if option == "e" then
-			if options then
-				option, options = split(",", options, 2)
-				if option then -- custom event
-					-- TODO: Handle custom event
-				end
+	local defaultedToTimedCount = 0
+
+	local option, rest = split(",", options, 2)
+	if option == "e" then
+		if rest then
+			local customEvent, _ = split(",", rest, 2)
+			if customEvent then
+				-- TODO: Handle custom event
+				tinsert(replaced, { reason = 1, string = option })
+				defaultedToTimedCount = defaultedToTimedCount + #assignments
 			end
-		elseif option:sub(1, 1) == "p" then
-			-- local _, phase = option:match(phaseNumberRegex)
-			-- if phase and phase ~= "" then
-			-- 	local phaseNumber = tonumber(phase, 10)
-			-- 	if phaseNumber then
-			-- 		for _, assignment in pairs(assignments) do
-			-- 			local phasedAssignment = PhasedAssignment:New(assignment)
-			-- 			phasedAssignment.time = time
-			-- 			phasedAssignment.phase = phaseNumber
-			-- 			tinsert(derivedAssignments, phasedAssignment)
-			-- 		end
-			-- 	end
-			-- 	regularTimer = false
-			-- end
-		else
-			local combatLogEventAbbreviation, spellIDStr, spellCountStr = split(":", option, 3)
-			if combatLogEventFromAbbreviation[combatLogEventAbbreviation] then
-				local spellID = tonumber(spellIDStr)
-				local spellCount = tonumber(spellCountStr)
-				if spellID then
-					local bossDungeonEncounterID = GetBossDungeonEncounterIDFromSpellID(spellID)
-					if bossDungeonEncounterID then
-						if spellCount then
-							if IsValidSpellCount(bossDungeonEncounterID, spellID, spellCount) then
-							else
-								spellCount = GetMaxSpellCount(bossDungeonEncounterID, spellID)
-							end
-						else
-							spellCount = 1
-						end
-						if spellCount then
-							for _, assignment in pairs(assignments) do
-								local combatLogEventAssignment = CombatLogEventAssignment:New(assignment)
-								combatLogEventAssignment.combatLogEventType = combatLogEventAbbreviation
-								combatLogEventAssignment.time = time
-								combatLogEventAssignment.spellCount = spellCount
-								combatLogEventAssignment.combatLogEventSpellID = spellID
-								tinsert(derivedAssignments, combatLogEventAssignment)
-							end
-							regularTimer = false
-						end
-					end
-				end
+		end
+	elseif option:sub(1, 1) == "p" then
+		tinsert(replaced, { reason = 1, string = option })
+		defaultedToTimedCount = defaultedToTimedCount + #assignments
+		-- local _, phase = option:match(phaseNumberRegex)
+		-- if phase and phase ~= "" then
+		-- 	local phaseNumber = tonumber(phase, 10)
+		-- 	if phaseNumber then
+		-- 		for _, assignment in pairs(assignments) do
+		-- 			local phasedAssignment = PhasedAssignment:New(assignment)
+		-- 			phasedAssignment.time = time
+		-- 			phasedAssignment.phase = phaseNumber
+		-- 			tinsert(derivedAssignments, phasedAssignment)
+		-- 		end
+		-- 	end
+		-- 	regularTimer = false
+		-- end
+	else
+		local success, replacedInvalidSpellCount, invalidCombatLogEventTypeOrCombatLogEventSpellID =
+			ProcessCombatEventLogEventOption(option, time, assignments, derivedAssignments, replaced, encounterIDs)
+		if success then
+			if replacedInvalidSpellCount then
+				return 0, #assignments
+			else
+				return 0, 0
 			end
+		elseif invalidCombatLogEventTypeOrCombatLogEventSpellID then
+			defaultedToTimedCount = defaultedToTimedCount + #assignments
 		end
 	end
 	if regularTimer then
@@ -190,6 +267,7 @@ local function ProcessOptions(assignments, derivedAssignments, time, options)
 			tinsert(derivedAssignments, timedAssignment)
 		end
 	end
+	return defaultedToTimedCount, 0
 end
 
 ---@param line string
@@ -205,6 +283,79 @@ local function ParseTime(line)
 	return time, options
 end
 
+---@param failedOrReplaced table<integer, FailureTableEntry>
+---@param failedCount integer
+---@param defaultedToTimedCount integer
+---@param defaultedSpellCount integer
+local function LogFailures(failedOrReplaced, failedCount, defaultedToTimedCount, defaultedSpellCount)
+	---@class InterfaceUpdater
+	local interfaceUpdater = Private.interfaceUpdater
+
+	if failedCount > 0 then
+		local msg
+		if failedCount == 1 then
+			msg = format("%s %d %s:", L["Failed to import"], failedCount, L["assignment"])
+		else
+			msg = format("%s %d %s:", L["Failed to import"], failedCount, L["assignments"])
+		end
+		interfaceUpdater.LogMessage(msg, 3, 1)
+
+		for _, value in ipairs(failedOrReplaced) do
+			if value.reason == 6 then
+				msg = format("%s: '%s'", L["Invalid assignee name or role"], value.string)
+				interfaceUpdater.LogMessage(msg, 3, 2)
+			end
+		end
+	end
+
+	if defaultedToTimedCount > 0 then
+		local msg
+		if defaultedToTimedCount == 1 then
+			msg = format("%d %s:", defaultedToTimedCount, L["assignment was defaulted to a timed assignment"])
+		else
+			msg = format("%d %s:", defaultedToTimedCount, L["assignments were defaulted to timed assignments"])
+		end
+		interfaceUpdater.LogMessage(msg, 2, 1)
+
+		for _, value in ipairs(failedOrReplaced) do
+			if value.reason == 1 then
+				msg = format("%s: '%s'.", L["Invalid assignment type"], value.string)
+				interfaceUpdater.LogMessage(msg, 2, 2)
+			elseif value.reason == 2 then
+				msg = format("%s: '%s'.", L["Invalid combat log event type"], value.string)
+				interfaceUpdater.LogMessage(msg, 2, 2)
+			elseif value.reason == 3 then
+				msg = format("%s: '%s'.", L["Invalid combat log event spell ID"], value.string)
+				interfaceUpdater.LogMessage(msg, 2, 2)
+			elseif value.reason == 7 then
+				msg = format("%s (%s): '%s'.", L["Invalid combat log event spell ID"], L["Wrong boss"], value.string)
+				interfaceUpdater.LogMessage(msg, 2, 2)
+			end
+		end
+	end
+
+	if defaultedSpellCount > 0 then
+		local msg
+		if defaultedSpellCount == 1 then
+			msg = format("%d %s:", defaultedSpellCount, L["assignment had its spell count replaced"])
+		else
+			msg = format("%d %s:", defaultedSpellCount, L["assignments had their spell counts replaced"])
+		end
+		interfaceUpdater.LogMessage(msg, 1, 1)
+
+		local assigned = L["Invalid spell count has been assigned the value"]
+		for _, value in ipairs(failedOrReplaced) do
+			if value.reason == 4 then
+				msg = format("'%s': %s '%d'.", value.string, assigned, value.replacedSpellCount)
+				interfaceUpdater.LogMessage(msg, 1, 2)
+			elseif value.reason == 5 then
+				msg = format("'%s': %s '1'.", value.string, assigned)
+				interfaceUpdater.LogMessage(msg, 1, 2)
+			end
+		end
+	end
+end
+
 -- Repopulates assignments for the note based on the note content. Returns a boss name if one was found using spellIDs
 -- in the text.
 ---@param plan Plan Plan to repopulate
@@ -212,24 +363,34 @@ end
 ---@return integer|nil
 local function ParseNote(plan, text)
 	wipe(plan.assignments)
-	local bossDungeonEncounterID = nil
+	local bossDungeonEncounterIDs = {} ---@type table<integer, {assignmentIDs: table<integer, integer>, string: string}>
+	local lowerPriorityEncounterIDs = {} ---@type table<integer, integer>
 	local otherContent = {}
+	local failedOrReplaced = {} ---@type table<integer, FailureTableEntry>
+	local failedCount, defaultedToTimedCount, defaultedSpellCount = 0, 0, 0
 
 	for _, line in pairs(text) do
 		local time, options = ParseTime(line)
 		if time and options then
-			local spellID, generalText = line:match(postOptionsPreDashRegex)
+			local spellID, _ = line:match(postOptionsPreDashRegex)
 			local spellIDNumber = nil
 			if spellID then
 				spellIDNumber = tonumber(spellID)
-				if not bossDungeonEncounterID and spellIDNumber then
-					bossDungeonEncounterID = GetBossDungeonEncounterIDFromSpellID(spellIDNumber)
+				if spellIDNumber then
+					local bossDungeonEncounterID = GetBossDungeonEncounterIDFromSpellID(spellIDNumber)
+					if bossDungeonEncounterID then
+						lowerPriorityEncounterIDs[bossDungeonEncounterID] = (
+							lowerPriorityEncounterIDs[bossDungeonEncounterID] or 0
+						) + 1
+					end
 				end
-			else
-				generalText = line:match(postOptionsPreDashNoSpellRegex)
 			end
-			local inputs = CreateAssignmentsFromLine(line)
-			ProcessOptions(inputs, plan.assignments, time, options)
+			local inputs, count = CreateAssignmentsFromLine(line, failedOrReplaced)
+			failedCount = failedCount + count
+			local defaultedToTimed, defaultedCombatLogAssignment =
+				ProcessOptions(inputs, plan.assignments, time, options, failedOrReplaced, bossDungeonEncounterIDs)
+			defaultedToTimedCount = defaultedToTimedCount + defaultedToTimed
+			defaultedSpellCount = defaultedSpellCount + defaultedCombatLogAssignment
 		else
 			if line:gsub("%s", ""):len() ~= 0 then
 				tinsert(otherContent, line)
@@ -239,21 +400,43 @@ local function ParseNote(plan, text)
 
 	plan.content = otherContent
 
-	if not bossDungeonEncounterID then
-		for _, assignment in ipairs(plan.assignments) do
-			if getmetatable(assignment) == CombatLogEventAssignment then
-				local maybeID = GetBossDungeonEncounterIDFromSpellID(
-					assignment--[[@as CombatLogEventAssignment]].combatLogEventSpellID
-				)
-				if maybeID then
-					bossDungeonEncounterID = maybeID
-					break
+	local determinedBossDungeonEncounterID, maxCount = nil, 0
+	for bossDungeonEncounterID, assignmentIDsAndOptions in pairs(bossDungeonEncounterIDs) do
+		local count = #assignmentIDsAndOptions.assignmentIDs
+		if count > maxCount then
+			maxCount = count
+			determinedBossDungeonEncounterID = bossDungeonEncounterID
+		end
+	end
+	if not determinedBossDungeonEncounterID then
+		for bossDungeonEncounterID, count in pairs(lowerPriorityEncounterIDs) do
+			if count > maxCount then
+				maxCount = count
+				determinedBossDungeonEncounterID = bossDungeonEncounterID
+			end
+		end
+	end
+
+	local FindAssignmentByUniqueID = utilities.FindAssignmentByUniqueID
+	-- Convert assignments not matching the determined boss dungeon encounter ID to timed assignments
+	for bossDungeonEncounterID, assignmentIDsAndOptions in pairs(bossDungeonEncounterIDs) do
+		if bossDungeonEncounterID ~= determinedBossDungeonEncounterID then
+			for _, assignmentID in pairs(assignmentIDsAndOptions.assignmentIDs) do
+				local assignment = FindAssignmentByUniqueID(plan.assignments, assignmentID)
+				if assignment then
+					assignment = TimedAssignment:New(assignment, true)
+					tinsert(failedOrReplaced, { reason = 7, string = assignmentIDsAndOptions.string })
+					defaultedToTimedCount = defaultedToTimedCount + 1
 				end
 			end
 		end
 	end
 
-	return bossDungeonEncounterID
+	if #failedOrReplaced > 0 then
+		LogFailures(failedOrReplaced, failedCount, defaultedToTimedCount, defaultedSpellCount)
+	end
+
+	return determinedBossDungeonEncounterID
 end
 
 ---@param assignment CombatLogEventAssignment|TimedAssignment
