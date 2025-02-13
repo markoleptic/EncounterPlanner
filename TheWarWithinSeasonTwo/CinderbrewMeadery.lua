b6b8881a4dfe0587c91ad32406b2a0935916aa72
markoleptic <markalexcham@gmail.com>
2025-02-12 22:57:07 -0700

    Implement Brew Master Aldryr and I'pa

--- TheWarWithinSeasonTwo/CinderbrewMeadery.lua
+++ TheWarWithinSeasonTwo/CinderbrewMeadery.lua
@@ -25,16 +25,129 @@ Private.raidInstances[2661] = RaidInstance:New({
 			journalEncounterID = 2586,
 			dungeonEncounterID = 2900,
 			instanceID = 2661,
-			abilities = {},
-			phases = {},
+			abilities = {
+				[442525] = BossAbility:New({ -- Happy Hour
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 27.8 },
+							signifiesPhaseEnd = true,
+						}),
+					},
+					duration = 20.0,
+					castTime = 2.0,
+				}),
+				[432198] = BossAbility:New({ -- Blazing Belch
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 14.4 },
+						}),
+					},
+					eventTriggers = {
+						[442525] = EventTrigger:New({ -- Happy Hour over
+							combatLogEventType = "SAR",
+							castTimes = { 17.6, 23.1, 23.1 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 3.0,
+				}),
+				[432179] = BossAbility:New({ -- Throw Cinderbrew
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 10.0 },
+						}),
+					},
+					eventTriggers = {
+						[442525] = EventTrigger:New({ -- Happy Hour over
+							combatLogEventType = "SAR",
+							castTimes = { 14.0, 18.2, 18.2 },
+						}),
+					},
+					duration = 9.0,
+					castTime = 1.5,
+				}),
+				[432229] = BossAbility:New({ -- Keg Smash
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 5.1 },
+						}),
+					},
+					eventTriggers = {
+						[442525] = EventTrigger:New({ -- Happy Hour over
+							combatLogEventType = "SAR",
+							castTimes = { 9.1, 14.5, 14.5, 14.5 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 1.5,
+				}),
+			},
+			phases = {
+				-- TODO: Add option for fixed first phase duration
+				[1] = BossPhase:New({
+					firstPhaseDuration = 27.8,
+					fixedDurationFirstPhaseDuration = true,
+					duration = 50.9,
+					defaultDuration = 50.9,
+					count = 3,
+					defaultCount = 3,
+					repeatAfter = 2,
+					name = "P1",
+				}),
+				[2] = BossPhase:New({
+					duration = 20.0,
+					defaultDuration = 20.0,
+					count = 3,
+					defaultCount = 3,
+					repeatAfter = 1,
+					name = "P2",
+				}),
+			},
 		}),
 		Boss:New({ -- I'pa
 			bossID = { 210267 },
 			journalEncounterID = 2587,
 			dungeonEncounterID = 2929,
 			instanceID = 2661,
-			abilities = {},
-			phases = {},
+			abilities = {
+				[439365] = BossAbility:New({ -- Spouting Stout
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 10.6, 47.3, 47.3, 47.3 },
+						}),
+					},
+					duration = 8.0,
+					castTime = 2.0,
+				}),
+				[439202] = BossAbility:New({ -- Burning Fermentation
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 35.0, 47.3, 47.3, 47.3 },
+						}),
+					},
+					duration = 16.0,
+					castTime = 2.0,
+				}),
+				[439031] = BossAbility:New({ -- Bottoms Uppercut
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 26.3, 47.3, 47.3, 47.3 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 3.0,
+				}),
+				[440147] = BossAbility:New({ -- Fill 'Er Up
+					-- TODO: Signify that this might not ever happen
+					phases = {},
+					duration = 0.0,
+					castTime = 0.0,
+				}),
+			},
+			[1] = BossPhase:New({
+				duration = 180.0,
+				defaultDuration = 180.0,
+			}),
 		}),
 		Boss:New({ -- Benk Buzzbee
 			bossID = { 218002 },
