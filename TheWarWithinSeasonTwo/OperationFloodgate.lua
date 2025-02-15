85c539d3098242d818fe48a6ad7b8f0fecfb9677
markoleptic <markalexcham@gmail.com>
2025-02-15 16:21:09 -0700

    Finish Operation Floodgate

--- TheWarWithinSeasonTwo/OperationFloodgate.lua
+++ TheWarWithinSeasonTwo/OperationFloodgate.lua
@@ -68,7 +68,8 @@ Private.raidInstances[2773] = RaidInstance:New({
 							castTimes = { 51.0 },
 						}),
 					},
-					duration = math.huge,
+					-- TODO: Implement support for lasting until end of phase
+					duration = 60,
 					castTime = 1.5,
 				}),
 			},
@@ -94,24 +95,186 @@ Private.raidInstances[2773] = RaidInstance:New({
 			journalEncounterID = 2649,
 			dungeonEncounterID = 3019,
 			instanceID = 2773,
-			abilities = {},
-			phases = {},
+			abilities = {
+				[459799] = BossAbility:New({ -- Wallop
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 6.1 },
+							repeatInterval = 17.0,
+						}),
+					},
+					duration = 0.0,
+					castTime = 1.5,
+				}),
+				[459779] = BossAbility:New({ -- Barreling Charge
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 22.8 },
+							repeatInterval = 17.0,
+						}),
+					},
+					duration = 2.0,
+					castTime = 3.5,
+				}),
+				[460867] = BossAbility:New({ -- Big Bada Boom
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 13.9 },
+							repeatInterval = 34.2,
+						}),
+					},
+					duration = 2.0,
+					castTime = 3.5,
+				}),
+				[1217653] = BossAbility:New({ -- B.B.B.F.G.
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 6.5 },
+							repeatInterval = 17.7,
+						}),
+					},
+					duration = 0.0,
+					castTime = 3.5,
+				}),
+				[473690] = BossAbility:New({ -- Kinetic Explosive Gel
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 17.7 },
+							repeatInterval = 17.7,
+						}),
+					},
+					duration = 0.0,
+					castTime = 2.0,
+				}),
+			},
+			-- TODO: Add option for phase 2 when one of the bosses dies
+			phases = {
+				[1] = BossPhase:New({
+					duration = 180.0,
+					defaultDuration = 180.0,
+					name = "P1",
+				}),
+			},
 		}),
 		Boss:New({ -- Swampface
 			bossID = { 226396 },
 			journalEncounterID = 2650,
 			dungeonEncounterID = 3053,
 			instanceID = 2773,
-			abilities = {},
-			phases = {},
+			abilities = {
+				[473070] = BossAbility:New({ -- Awaken the Swamp
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 19.0 },
+							repeatInterval = 30.0,
+						}),
+					},
+					duration = 4.0,
+					castTime = 4.0,
+				}),
+				[473114] = BossAbility:New({ -- Mudslide
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 9.0 },
+							repeatInterval = 30.0,
+						}),
+					},
+					duration = 0.0,
+					castTime = 0.0,
+				}),
+				[469478] = BossAbility:New({ -- Sludge Claws
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 3.0 },
+							repeatInterval = 30.0,
+						}),
+					},
+					duration = 0.0,
+					castTime = 2.5,
+				}),
+				[470039] = BossAbility:New({ -- Razorchoke Vines
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 1.0 },
+							repeatInterval = 30.0,
+						}),
+					},
+					duration = 24.0,
+					castTime = 0.0,
+				}),
+			},
+			phases = {
+				[1] = BossPhase:New({
+					duration = 180.0,
+					defaultDuration = 180.0,
+					name = "P1",
+				}),
+			},
 		}),
 		Boss:New({ -- Geezle Gigazap
 			bossID = { 226404 },
 			journalEncounterID = 2651,
 			dungeonEncounterID = 3054,
 			instanceID = 2773,
-			abilities = {},
-			phases = {},
+			abilities = {
+				[465463] = BossAbility:New({ -- Turbo Charge
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 1.6 },
+							repeatInterval = 60.0,
+						}),
+					},
+					duration = 10.0,
+					castTime = 0.0,
+				}),
+				[468841] = BossAbility:New({ -- Leaping Sparks
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 38.0 },
+							repeatInterval = 60.0,
+						}),
+					},
+					duration = 8.0,
+					castTime = 0.0,
+				}),
+				[468813] = BossAbility:New({ -- Gigazap
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 28.0 },
+							repeatInterval = { 26.0, 34.0 },
+						}),
+					},
+					duration = 8.0,
+					castTime = 0.0,
+				}),
+				[466190] = BossAbility:New({ -- Thunder Punch
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 24.0 },
+							repeatInterval = { 26.0, 34.0 },
+						}),
+					},
+					duration = 4.0,
+					castTime = 0.0,
+				}),
+				[468723] = BossAbility:New({ -- Shock Water
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 0.0 },
+							repeatInterval = 0.0,
+						}),
+					},
+					duration = 4.0,
+					castTime = 0.0,
+				}),
+			},
+			phases = {
+				[1] = BossPhase:New({
+					duration = 180.0,
+					defaultDuration = 180.0,
+					name = "P1",
+				}),
+			},
 		}),
 	},
 })
