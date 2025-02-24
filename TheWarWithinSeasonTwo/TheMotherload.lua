1da59606481b59409e484b9afff757c1facf0428
markoleptic <markalexcham@gmail.com>
2025-02-23 19:20:24 -0700

    Update The Motherload

--- TheWarWithinSeasonTwo/TheMotherload.lua
+++ TheWarWithinSeasonTwo/TheMotherload.lua
@@ -35,61 +35,58 @@ Private.dungeonInstances[1594] = DungeonInstance:New({
 				[269493] = BossAbility:New({ -- Footbomb Launcher
 					phases = {
 						[1] = BossAbilityPhase:New({
-							castTimes = { 19.1 },
-							repeatInterval = 47.4,
+							castTimes = { 19.1, 48.1 },
+							repeatInterval = 43.7,
 						}),
 					},
 					duration = 15.0,
 					castTime = 2.0,
-				}),
-				[256493] = BossAbility:New({ -- Blazing Azerite
-					phases = {
-						[1] = BossAbilityPhase:New({
-							castTimes = { 0.0 },
-						}),
-					},
-					duration = 0.0,
-					castTime = 0.0,
+					allowedCombatLogEventTypes = { "SCC" },
 				}),
 				[271903] = BossAbility:New({ -- Coin Magnet
 					phases = {
 						[1] = BossAbilityPhase:New({
 							castTimes = { 41.0 },
-							repeatInterval = 43.8,
+							repeatInterval = 43.7,
 						}),
 					},
-					duration = 0.0,
-					castTime = 0.0,
+					duration = 4.0,
+					castTime = 2.5,
+					allowedCombatLogEventTypes = { "SCS", "SCC", "SAA", "SAR" },
 				}),
 				[262347] = BossAbility:New({ -- Static Pulse
 					phases = {
 						[1] = BossAbilityPhase:New({
-							castTimes = { 6.1 },
-							repeatInterval = 43.8,
+							castTimes = { 6.4, 48.1 },
+							repeatInterval = 43.7,
 						}),
 					},
 					duration = 2.5,
 					castTime = 8.0,
+					durationHurts = true,
+					allowedCombatLogEventTypes = { "SCS", "SCC" },
 				}),
 				[1217294] = BossAbility:New({ -- Shocking Claw
 					phases = {
 						[1] = BossAbilityPhase:New({
-							castTimes = { 30.0 },
-							repeatInterval = 48.6,
+							castTimes = { 30.0, 48.6 },
+							repeatInterval = 43.7,
 						}),
 					},
 					duration = 3.0,
 					castTime = 4.0,
+					allowedCombatLogEventTypes = { "SCS", "SCC" },
 				}),
 				[271784] = BossAbility:New({ -- Throw Coins
 					phases = {
 						[1] = BossAbilityPhase:New({
-							castTimes = { 13.0, 2.0, 2.0, 2.0, 2.0 },
-							repeatInterval = { 43.8, 2.0, 2.0, 2.0, 2.0 },
+							castTimes = { 12.5, 2.0, 2.0, 2.0, 2.0 },
+							repeatInterval = { 41.3, 2.0, 2.0, 2.0, 2.0 },
 						}),
 					},
 					duration = 0.0,
 					castTime = 1.5,
+					allowedCombatLogEventTypes = {}, -- Unreliable cast amounts
 				}),
 			},
 			phases = {
@@ -112,22 +109,24 @@ Private.dungeonInstances[1594] = DungeonInstance:New({
 				[271698] = BossAbility:New({ -- Azerite Infusion
 					phases = {
 						[1] = BossAbilityPhase:New({
-							castTimes = { 9.6 },
-							repeatInterval = 44.0,
+							castTimes = { 5.1 },
+							repeatInterval = 42.5,
 						}),
 					},
 					duration = 0.0,
 					castTime = 1.0,
+					allowedCombatLogEventTypes = { "SCS", "SCC" },
 				}),
 				[258622] = BossAbility:New({ -- Resonant Quake
 					phases = {
 						[1] = BossAbilityPhase:New({
-							castTimes = { 29.0 },
-							repeatInterval = 41.3,
+							castTimes = { 32.3 },
+							repeatInterval = 42.5,
 						}),
 					},
 					duration = 6.0,
 					castTime = 5.0,
+					allowedCombatLogEventTypes = { "SCS", "SCC" },
 				}),
 				[257593] = BossAbility:New({ -- Call Earthrager
 					phases = {
@@ -138,16 +137,18 @@ Private.dungeonInstances[1594] = DungeonInstance:New({
 					},
 					duration = 0.0,
 					castTime = 2.5,
+					allowedCombatLogEventTypes = { "SCS", "SCC" },
 				}),
 				[275907] = BossAbility:New({ -- Tectonic Smash
 					phases = {
 						[1] = BossAbilityPhase:New({
-							castTimes = { 5.1, 19.4, 21.5 },
-							repeatInterval = { 19.4, 23.1 },
+							castTimes = { 11.7 },
+							repeatInterval = { 15.8, 26.7 },
 						}),
 					},
 					duration = 0.0,
 					castTime = 3.5,
+					allowedCombatLogEventTypes = { "SCS", "SCC" },
 				}),
 			},
 			phases = {
@@ -164,16 +165,6 @@ Private.dungeonInstances[1594] = DungeonInstance:New({
 			dungeonEncounterID = 2107,
 			instanceID = 1594,
 			abilities = {
-				[259856] = BossAbility:New({ -- Chemical Burn
-					phases = {
-						[1] = BossAbilityPhase:New({
-							castTimes = { 14.5 },
-							repeatInterval = { 15.0, 27.0 },
-						}),
-					},
-					duration = 0.0,
-					castTime = 0.0,
-				}),
 				[275992] = BossAbility:New({ -- Gushing Catalyst
 					phases = {
 						[1] = BossAbilityPhase:New({
@@ -183,16 +174,18 @@ Private.dungeonInstances[1594] = DungeonInstance:New({
 					},
 					duration = 0.0,
 					castTime = 0.0,
+					allowedCombatLogEventTypes = { "SCC" },
 				}),
 				[270042] = BossAbility:New({ -- Azerite Catalyst
 					phases = {
 						[1] = BossAbilityPhase:New({
-							castTimes = { 10.0 },
+							castTimes = { 10.5 },
 							repeatInterval = { 53.0 },
 						}),
 					},
 					duration = 3.0,
 					castTime = 2.5,
+					allowedCombatLogEventTypes = { "SCS", "SCC" },
 				}),
 				[259940] = BossAbility:New({ -- Propellant Blast
 					phases = {
@@ -203,6 +196,7 @@ Private.dungeonInstances[1594] = DungeonInstance:New({
 					},
 					duration = 4.0,
 					castTime = 3.5,
+					allowedCombatLogEventTypes = { "SCS", "SCC" },
 				}),
 			},
 			phases = {
@@ -218,16 +212,26 @@ Private.dungeonInstances[1594] = DungeonInstance:New({
 			journalEncounterID = 2116,
 			dungeonEncounterID = 2108,
 			instanceID = 1594,
+			preferredCombatLogEventAbilities = {
+				[1] = nil,
+				[2] = { combatLogEventSpellID = 260189, combatLogEventType = "SCC" },
+				[3] = { combatLogEventSpellID = 260190, combatLogEventType = "SCC" },
+			},
 			abilities = {
 				[260280] = BossAbility:New({ -- Gatling Gun
 					phases = {
 						[1] = BossAbilityPhase:New({
-							castTimes = { 15.0 },
+							castTimes = { 20.0 },
 							repeatInterval = 30.0,
+						}),
+						[3] = BossAbilityPhase:New({
+							castTimes = { 17.1 },
+							repeatInterval = { 20.0, 25.0 },
 						}),
 					},
 					duration = 8.0,
 					castTime = 3.0,
+					allowedCombatLogEventTypes = {}, -- P3 cast times could be different depending on phase timings
 				}),
 				[260829] = BossAbility:New({ -- Homing Missile
 					phases = {
@@ -235,9 +239,14 @@ Private.dungeonInstances[1594] = DungeonInstance:New({
 							castTimes = { 5.0 },
 							repeatInterval = 30.0,
 						}),
+						[3] = BossAbilityPhase:New({
+							castTimes = { 7.1 },
+							repeatInterval = { 21.0, 24.0 },
+						}),
 					},
 					duration = 6.0,
 					castTime = 3.0,
+					allowedCombatLogEventTypes = {}, -- P3 cast times could be different depending on phase timings
 				}),
 				[276229] = BossAbility:New({ -- Micro Missiles
 					phases = {
@@ -245,19 +254,26 @@ Private.dungeonInstances[1594] = DungeonInstance:New({
 							castTimes = { 10.0 },
 							repeatInterval = 30.0,
 						}),
+						[3] = BossAbilityPhase:New({
+							castTimes = { 11.8 },
+							repeatInterval = 19.4, -- Inconsistent
+						}),
 					},
 					duration = 0.0,
 					castTime = 5.0,
+					allowedCombatLogEventTypes = {}, -- P3 cast times could be different depending on phase timings
+					buffer = 1.0,
 				}),
 				[271456] = BossAbility:New({ -- Drill Smash
 					eventTriggers = {
 						[260189] = EventTrigger:New({ -- Configuration: Drill
 							combatLogEventType = "SCC",
-							castTimes = { 17.5, 11.0, 11.0 },
+							castTimes = { 17.5, 11.0, 11.0, 11.0 },
 						}),
 					},
 					duration = 0.0,
 					castTime = 5.0,
+					allowedCombatLogEventTypes = { "SCS", "SCC" },
 				}),
 				[260189] = BossAbility:New({ -- Configuration: Drill
 					phases = {
@@ -268,35 +284,37 @@ Private.dungeonInstances[1594] = DungeonInstance:New({
 					},
 					duration = 0.0,
 					castTime = 0.0,
+					allowedCombatLogEventTypes = { "SCC" },
 				}),
 				[260190] = BossAbility:New({ -- Configuration: Combat
 					phases = {
-						[2] = BossAbilityPhase:New({
-							castTimes = { 43.5 },
-							signifiesPhaseEnd = true,
+						[3] = BossAbilityPhase:New({
+							castTimes = { 0.0 },
+							signifiesPhaseStart = true,
 						}),
 					},
 					duration = 0.0,
 					castTime = 0.0,
+					allowedCombatLogEventTypes = { "SCC" },
 				}),
 			},
 			phases = {
 				[1] = BossPhase:New({
 					duration = 90.0,
 					defaultDuration = 90.0,
-					count = 2,
-					defaultCount = 2,
 					name = "P1",
-					repeatAfter = 2,
 					fixedCount = true,
 				}),
 				[2] = BossPhase:New({
-					duration = 43.5,
-					defaultDuration = 43.5,
+					duration = 60.0,
+					defaultDuration = 60.0,
 					name = "P2",
-					count = 1,
-					defaultCount = 1,
-					repeatAfter = 1,
+					fixedCount = true,
+				}),
+				[3] = BossPhase:New({
+					duration = 90.0,
+					defaultDuration = 90.0,
+					name = "P1",
 					fixedCount = true,
 				}),
 			},
