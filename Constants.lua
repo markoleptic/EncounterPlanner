a6986eec145a1dd2430eb6c42f135997cd82dcd2
markoleptic <markalexcham@gmail.com>
2026-02-23 20:00:09 -0700

    Update WoW version

--- Constants.lua
+++ Constants.lua
@@ -127,12 +127,5 @@ if not isElevenDotTwo then
 end
 
 Private.constants.kPatchNotesText = [[
--   Added the ability to manually add spells to the spell database
-    -   The Cooldown Overrides preferences tab has been renamed to Spells.
-    -   Added new section to Spells tab: Manually Added Spells
-        -   Click the + button and type a valid spell ID to add a spell.
-        -   A spell category and the class/role(s) which can use the spell must be specified. The Core category and your class/role are chosen by default.
-        -   Spells under the Racial and Consumable categories do not require a class or role.
--   Fixed an issue where spell category names were using the English version instead of the localized version.
--   Fixed an issue where items would not show as favorited when the assignee was a role, group number, type, or everyone.
+-   Update WoW version.
 ]]
