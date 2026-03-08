b8e1d33349fee4289dfdc51fb6a12cefc0b5a8c3
markoleptic <markalexcham@gmail.com>
2026-03-07 18:26:50 -0700

    Add abilities for first two dungeons

--- MidnightSeasonOne/AlgetharAcademy.lua
+++ MidnightSeasonOne/AlgetharAcademy.lua
@@ -28,8 +28,58 @@ Private.dungeonInstances[2526] = DungeonInstance:New({
 			journalEncounterID = 2509,
 			dungeonEncounterID = 2562,
 			instanceID = 2526,
-			abilities = {},
-			phases = {},
+			abilities = {
+				[385958] = BossAbility:New({ -- Arcane Expulsion
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 5.0 },
+							repeatInterval = { 18.0, 26.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 4.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[386173] = BossAbility:New({ -- Mana Bombs
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 15.0 },
+							repeatInterval = { 18.0, 26.0 },
+						}),
+					},
+					duration = 4.0,
+					castTime = 2.5,
+					allowedCombatLogEventTypes = {},
+				}),
+				[387691] = BossAbility:New({ -- Arcane Orbs
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 2.0 },
+							repeatInterval = { 18.0, 26.0 },
+						}),
+					},
+					duration = 10.0,
+					castTime = 0.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[388537] = BossAbility:New({ -- Arcane Fissure
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 40.0 },
+							repeatInterval = { 44.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 3.0,
+					allowedCombatLogEventTypes = {},
+				}),
+			},
+			phases = {
+				[1] = BossPhase:New({
+					duration = 180.0,
+					defaultDuration = 180.0,
+				}),
+			},
 		}),
 		Boss:New({ -- Overgrown Ancient
 			bossIDs = {
@@ -45,8 +95,58 @@ Private.dungeonInstances[2526] = DungeonInstance:New({
 			journalEncounterID = 2512,
 			dungeonEncounterID = 2563,
 			instanceID = 2526,
-			abilities = {},
-			phases = {},
+			abilities = {
+				[388544] = BossAbility:New({ -- Barkbreaker
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 9.0 },
+							repeatInterval = { 28.0 },
+						}),
+					},
+					duration = 9.0,
+					castTime = 1.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[388623] = BossAbility:New({ -- Branch Out
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 30.0 },
+							repeatInterval = { 56.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 2.5,
+					allowedCombatLogEventTypes = {},
+				}),
+				[388796] = BossAbility:New({ -- Germinate
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 18.0 },
+							repeatInterval = { 33.0, 23.0 },
+						}),
+					},
+					duration = 4.0,
+					castTime = 0.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[388923] = BossAbility:New({ -- Burst Forth
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 55.1 },
+							repeatInterval = { 56.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 2.0,
+					allowedCombatLogEventTypes = {},
+				}),
+			},
+			phases = {
+				[1] = BossPhase:New({
+					duration = 180.0,
+					defaultDuration = 180.0,
+				}),
+			},
 		}),
 		Boss:New({ -- Crawth
 			bossIDs = { 191736 },
@@ -56,8 +156,120 @@ Private.dungeonInstances[2526] = DungeonInstance:New({
 			journalEncounterID = 2495,
 			dungeonEncounterID = 2564,
 			instanceID = 2526,
-			abilities = {},
-			phases = {},
+			abilities = {
+				[376997] = BossAbility:New({ -- Savage Peck
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 5.0 },
+							repeatInterval = { 24.0 },
+						}),
+						[2] = BossAbilityPhase:New({
+							castTimes = { 22.9 },
+							repeatInterval = { 24.0 },
+						}),
+						[3] = BossAbilityPhase:New({
+							castTimes = { 32.8 },
+							repeatInterval = { 24.0 },
+						}),
+					},
+					duration = 10.0,
+					castTime = 4.0,
+					tankAbility = true,
+					allowedCombatLogEventTypes = {},
+				}),
+				[377004] = BossAbility:New({ -- Deafening Screech
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 14.0 },
+							repeatInterval = { 24.0 },
+						}),
+						[2] = BossAbilityPhase:New({
+							castTimes = { 31.8 },
+							repeatInterval = { 24.0 },
+						}),
+						[3] = BossAbilityPhase:New({
+							castTimes = { 41.8 },
+							repeatInterval = { 24.0 },
+						}),
+					},
+					duration = 8.0,
+					castTime = 2.5,
+					allowedCombatLogEventTypes = {},
+				}),
+				[377034] = BossAbility:New({ -- Overpowering Gust
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 20.0 },
+							repeatInterval = { 24.0 },
+						}),
+						[2] = BossAbilityPhase:New({
+							castTimes = { 37.8 },
+							repeatInterval = { 24.0 },
+						}),
+						[3] = BossAbilityPhase:New({
+							castTimes = { 47.8 },
+							repeatInterval = { 24.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 3.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1276752] = BossAbility:New({ -- Ruinous Winds
+					phases = {
+						[2] = BossAbilityPhase:New({
+							castTimes = { 0.0 },
+							signifiesPhaseStart = true,
+							signifiesPhaseEnd = true,
+						}),
+						[3] = BossAbilityPhase:New({
+							castTimes = { 0.0 },
+							signifiesPhaseStart = true,
+							signifiesPhaseEnd = true,
+						}),
+					},
+					duration = 0.0,
+					castTime = 20.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1285508] = BossAbility:New({ -- Blistering Fire
+					phases = {
+						[3] = BossAbilityPhase:New({
+							castTimes = { 15.9 },
+						}),
+					},
+					duration = 0.0,
+					durationLastsUntilEndOfPhase = true,
+					castTime = 00.0,
+					allowedCombatLogEventTypes = {},
+				}),
+			},
+			phases = {
+				[1] = BossPhase:New({
+					duration = 48.0,
+					defaultDuration = 48.0,
+					count = 1,
+					defaultCount = 1,
+					fixedCount = true,
+					name = "P1",
+				}),
+				[2] = BossPhase:New({
+					duration = 60.0,
+					defaultDuration = 60.0,
+					count = 1,
+					defaultCount = 1,
+					fixedCount = true,
+					name = "P2 (75% Health)",
+				}),
+				[3] = BossPhase:New({
+					duration = 72.0,
+					defaultDuration = 72.0,
+					count = 1,
+					defaultCount = 1,
+					fixedCount = true,
+					name = "P3 (45% Health)",
+				}),
+			},
 		}),
 		Boss:New({ -- Echo of Doragosa
 			bossIDs = { 190609 },
@@ -67,8 +279,46 @@ Private.dungeonInstances[2526] = DungeonInstance:New({
 			journalEncounterID = 2514,
 			dungeonEncounterID = 2565,
 			instanceID = 2526,
-			abilities = {},
-			phases = {},
+			abilities = {
+				[374343] = BossAbility:New({ -- Energy Bomb
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 14.0 },
+							repeatInterval = { 33.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 1.5,
+					allowedCombatLogEventTypes = {},
+				}),
+				[388822] = BossAbility:New({ -- Power Vacuum
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 30.0 },
+							repeatInterval = { 33.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 4.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[439488] = BossAbility:New({ -- Unleash Energy
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 0.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 2.0,
+					allowedCombatLogEventTypes = {},
+				}),
+			},
+			phases = {
+				[1] = BossPhase:New({
+					duration = 180.0,
+					defaultDuration = 180.0,
+				}),
+			},
 		}),
 	},
 })
--- MidnightSeasonOne/MagistersTerrace.lua
+++ MidnightSeasonOne/MagistersTerrace.lua
@@ -26,8 +26,47 @@ Private.dungeonInstances[2811] = DungeonInstance:New({
 			journalEncounterID = 2659,
 			dungeonEncounterID = 3071,
 			instanceID = 2811,
-			abilities = {},
-			phases = {},
+			abilities = {
+				[1214081] = BossAbility:New({ -- Arcane Expulsion
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 15.9 },
+							repeatInterval = { 23.1, 45.6 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 3.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[474345] = BossAbility:New({ -- Refueling Protocol
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 45.8 },
+							repeatInterval = { 68.5 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 3.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1214038] = BossAbility:New({ -- Ethereal Shackles
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 23.9 },
+							repeatInterval = { 23.1, 62.8 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 3.0,
+					allowedCombatLogEventTypes = {},
+				}),
+			},
+			phases = {
+				[1] = BossPhase:New({
+					duration = 180.0,
+					defaultDuration = 180.0,
+				}),
+			},
 		}),
 		Boss:New({ -- Seranel Sunlash
 			bossIDs = { 231863 },
@@ -37,8 +76,59 @@ Private.dungeonInstances[2811] = DungeonInstance:New({
 			journalEncounterID = 2661,
 			dungeonEncounterID = 3072,
 			instanceID = 2811,
-			abilities = {},
-			phases = {},
+			abilities = {
+				[1224903] = BossAbility:New({ -- Suppression Zone
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 17.8 },
+							repeatInterval = { 57.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 3.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1225792] = BossAbility:New({ -- Runic Mark
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 7.3 },
+							repeatInterval = { 29.2, 27.3, 29.1, 27.9 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 3.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1248689] = BossAbility:New({ -- Hastening Ward
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 26.7 },
+							repeatInterval = { 56.6, 57.0, 57.1 },
+						}),
+					},
+					duration = 15.0,
+					castTime = 0.0,
+					tankAbility = true,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1225193] = BossAbility:New({ -- Wave of Silence
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 51.7 },
+							repeatInterval = { 57.0 },
+						}),
+					},
+					duration = 8.0,
+					castTime = 5.0,
+					allowedCombatLogEventTypes = {},
+				}),
+			},
+			phases = {
+				[1] = BossPhase:New({
+					duration = 180.0,
+					defaultDuration = 180.0,
+				}),
+			},
 		}),
 		Boss:New({ -- Gemellus
 			bossIDs = { 231864 },
@@ -48,8 +138,57 @@ Private.dungeonInstances[2811] = DungeonInstance:New({
 			journalEncounterID = 2660,
 			dungeonEncounterID = 3073,
 			instanceID = 2811,
-			abilities = {},
-			phases = {},
+			abilities = {
+				[1224299] = BossAbility:New({ -- Astral Grasp
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 37.5 },
+							repeatInterval = { 43.0 },
+						}),
+					},
+					duration = 9.0,
+					castTime = 4.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1223847] = BossAbility:New({ -- Triplicate
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 6.0, 82.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 2.5,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1284954] = BossAbility:New({ -- Cosmic Sting
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 14.2 },
+							repeatInterval = { 43.0 },
+						}),
+					},
+					duration = 6.0,
+					castTime = 4.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1253709] = BossAbility:New({ -- Neural Link
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 25.1 },
+							repeatInterval = { 43.0 },
+						}),
+					},
+					duration = 12.0,
+					castTime = 2.0,
+					allowedCombatLogEventTypes = {},
+				}),
+			},
+			phases = {
+				[1] = BossPhase:New({
+					duration = 180.0,
+					defaultDuration = 180.0,
+				}),
+			},
 		}),
 		Boss:New({ -- Degentrius
 			bossIDs = { 231865 },
@@ -59,8 +198,58 @@ Private.dungeonInstances[2811] = DungeonInstance:New({
 			journalEncounterID = 2662,
 			dungeonEncounterID = 3074,
 			instanceID = 2811,
-			abilities = {},
-			phases = {},
+			abilities = {
+				[1215087] = BossAbility:New({ -- Unstable Void Essence
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 15.8 },
+							repeatInterval = { 23.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 2.5,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1280113] = BossAbility:New({ -- Hulking Fragment
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 3.6 },
+							repeatInterval = { 23.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 3.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1284627] = BossAbility:New({ -- Umbral Splinters
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 6.6 },
+							repeatInterval = { 23.0 },
+						}),
+					},
+					duration = 30.0,
+					castTime = 0.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1215897] = BossAbility:New({ -- Devouring Entropy
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 10.0, 0.1, 0.1 },
+							repeatInterval = { 22.7, 0.1, 0.1 },
+						}),
+					},
+					duration = 16.0,
+					castTime = 0.0,
+					allowedCombatLogEventTypes = {},
+				}),
+			},
+			phases = {
+				[1] = BossPhase:New({
+					duration = 180.0,
+					defaultDuration = 180.0,
+				}),
+			},
 		}),
 	},
 })
