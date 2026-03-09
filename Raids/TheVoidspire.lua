fa8f29d047b5714dcdaa13e33ee395d2966f2072
markoleptic <markalexcham@gmail.com>
2026-03-08 18:03:32 -0600

    Add abilities for first boss of voidspire

--- Raids/TheVoidspire.lua
+++ Raids/TheVoidspire.lua
@@ -43,10 +43,258 @@ Private.dungeonInstances[2912] = DungeonInstance:New({
 			journalEncounterID = 2733,
 			dungeonEncounterID = 3176,
 			instanceID = 2912,
-			abilities = {},
-			phases = {},
-			abilitiesHeroic = {},
-			phasesHeroic = {},
+			abilities = {
+				[1249251] = BossAbility:New({ -- Dark Upheaval
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 4.0 },
+							repeatInterval = { 48.0, 36.0, 102.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 2.5,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1249266] = BossAbility:New({ -- Umbral Collapse
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 32.0 },
+							repeatInterval = { 80.2, 105.8, 80.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 5.5,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1260206] = BossAbility:New({ -- Umbral Collapse 2 ??
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 39.5 },
+							repeatInterval = { 80.2, 105.8, 80.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 5.5,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1249714] = BossAbility:New({ -- Umbral Barrier
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 17.1 },
+							repeatInterval = { 80.0, 106.0, 80.0, 104.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 0.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1251361] = BossAbility:New({ -- Shadow's Advance
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 94.0 },
+							repeatInterval = { 106.0, 80.0, 104.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 3.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1255702] = BossAbility:New({ -- Void Fall
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 160.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 6.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1260712] = BossAbility:New({ -- Oblivion's Wrath
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 60.0 },
+							repeatInterval = { 18.0, 168.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 3.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1262036] = BossAbility:New({ -- Void Rupture
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 17.6 },
+							repeatInterval = { 80.0, 106.0, 80.0, 104.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 35.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1261249] = BossAbility:New({ -- Void Rupture 2
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 52.6 },
+							repeatInterval = { 80.0, 106.0, 80.0, 104.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 5.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1262776] = BossAbility:New({ -- Shadow's Advance
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 14.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 3.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1280015] = BossAbility:New({ -- Void Marked
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 20.0 },
+							repeatInterval = { 80.0, 106.0, 80.0, 104.0 },
+						}),
+					},
+					duration = 25.0,
+					castTime = 3.0,
+					allowedCombatLogEventTypes = {},
+				}),
+			},
+			phases = {
+				[1] = BossPhase:New({
+					duration = 360.0,
+					defaultDuration = 360.0,
+				}),
+			},
+			abilitiesHeroic = {
+				[1249251] = BossAbility:New({ -- Dark Upheaval
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 4.2 },
+							repeatInterval = { 36.0, 36.0, 79.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 2.5,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1249266] = BossAbility:New({ -- Umbral Collapse
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 18.3 },
+							repeatInterval = { 72.0, 79.0, 72.0, 77.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 5.5,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1260206] = BossAbility:New({ -- Umbral Collapse 2??
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 25.6 },
+							repeatInterval = { 72.0, 79.0, 72.0, 77.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 5.5,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1249714] = BossAbility:New({ -- Umbral Barrier
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 15.5 },
+							repeatInterval = { 71.5, 79.0, 72.0, 77.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 0.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1251361] = BossAbility:New({ -- Shadow's Advance
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 84.0 },
+							repeatInterval = { 79.0, 72.0, 77.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 3.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1255702] = BossAbility:New({ -- Void Fall
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 125.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 6.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1260712] = BossAbility:New({ -- Oblivion's Wrath
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 48.0 },
+							repeatInterval = { 18.1, 132.9 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 3.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1262036] = BossAbility:New({ -- Void Rupture
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 16.0 },
+							repeatInterval = { 71.6, 79.0, 72.0, 77.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 35.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1262776] = BossAbility:New({ -- Shadow's Advance
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 12.0 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 3.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				[1270946] = BossAbility:New({ -- Desolation
+					phases = {
+						[1] = BossAbilityPhase:New({
+							castTimes = { 58.1, 150.9 },
+						}),
+					},
+					duration = 0.0,
+					castTime = 7.0,
+					allowedCombatLogEventTypes = {},
+				}),
+				-- [1251583] = BossAbility:New({ -- March of the Endless
+				-- 	phases = {
+				-- 		[1] = BossAbilityPhase:New({
+				-- 			castTimes = { 336.6 },
+				-- 		}),
+				-- 	},
+				-- 	duration = 0.0,
+				-- 	castTime = 3.0,
+				-- 	allowedCombatLogEventTypes = {},
+				-- }),
+			},
+			phasesHeroic = {
+				[1] = BossPhase:New({
+					duration = 339.6,
+					defaultDuration = 339.6,
+				}),
+			},
 		}),
 		Boss:New({ -- Vorasius
 			bossIDs = {
