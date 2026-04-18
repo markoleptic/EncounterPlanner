0b22e9d9d931d5b89a1fe7a114af136eb2f11a78
markoleptic <markalexcham@gmail.com>
2026-04-17 22:25:35 -0600

    Update change log and version

--- Constants.lua
+++ Constants.lua
@@ -127,11 +127,5 @@ if not isElevenDotTwo then
 end
 
 Private.constants.kPatchNotesText = [[
--   Added two new mythic bosses: Vaelgor & Ezzorak and Chimaerus
--   Updated heroic timers for Averzian, Vaelgor & Ezzorak, Fallen-King Salhadaar, and Lightblinded Vanguard.
--   Added Nullbeam and Shadowmark to Vaelgor & Ezzorak.
--   Added Searing Radiance, Tyr's Wrath, and Divine Storm to heroic Lightblinded Vanguard.
--   Added Focused Aggression to mythic Vorasius.
--   Shortened the duration of Dread Breath and Gloom for improved visibility (Vaelgor & Ezzorak).
--   Removed abilities that appeared on PTR but do not appear on live.
+-   Added ability to import and export manually added spells.
 ]]
